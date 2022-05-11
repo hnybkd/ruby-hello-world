b117da1b53a265394ad0b1edeac699b81e9f777d
hnybkd <hny_bkd@protonmail.com>
2022-05-11 13:25:31 -0400

    refactored to meet style guide

--- ri20min.rb
+++ ri20min.rb
@@ -3,16 +3,16 @@
 class MegaGreeter
   attr_accessor :names
 
-# Create the object
-  def initialize(names = "World")
+  # Create the object
+  def initialize(names = 'World')
     @names = names
   end
 
   # Say hi to everybody
   def say_hi
     if @names.nil?
-      puts "..."
-    elsif @names.respond_to?("each")
+      puts '...'
+    elsif @names.respond_to?('each')
       # @names is a list of some kind, iterate!
       @names.each do |name|
         puts "Hello #{name}!"
@@ -25,10 +25,10 @@ class MegaGreeter
   # Say by to everybody
   def say_bye
     if @names.nil?
-      puts "..."
-    elsif @names.respond_to?("join")
+      puts '...'
+    elsif @names.respond_to?('join')
       # Join the list elements with commas
-      puts "Goodbye #{@names.join(", ")}.  Come back soon!"
+      puts "Goodbye #{@names.join(', ')}.  Come back soon!"
     else
       puts "Goodbye #{@names}.  Come back soon!"
     end
@@ -40,17 +40,17 @@ if __FILE__ == $0
   mg.say_hi
   mg.say_bye
 
-# Change name to be "Zeke"
-  mg.names = "Zeke"
+  # Change name to be "Zeke"
+  mg.names = 'Zeke'
   mg.say_hi
   mg.say_bye
 
-# Change the name to an array of names
-  mg.names = ["Alebert", "Brenda", "Charles", "Dave", "Engelbert"]
+  # Change the name to an array of names
+  mg.names = ['Alebert', 'Brenda', 'Charles', 'Dave', 'Engelbert']
   mg.say_hi
   mg.say_bye
 
-#Change to nil
+  # Change to nil
   mg.names = nil
   mg.say_hi
   mg.say_bye
